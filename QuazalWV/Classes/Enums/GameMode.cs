0c72e0cb1f25e52a5abb22dd486049309fe8dd35
michal-kapala <mim4k1@gmail.com>
2025-05-21 16:20:37 +0200

    gameses: add missing enum values

--- QuazalWV/Classes/Enums/GameMode.cs
+++ QuazalWV/Classes/Enums/GameMode.cs
@@ -1,18 +1,19 @@
 ﻿namespace QuazalWV
 {
-	/// <summary>
-	/// Available game mode IDs included in session data.
-	/// </summary>
-	public enum GameMode
-	{
-		ChestCapture = 2,
-		Alliance = 3,
-		Wanted = 4,
-		Intro = 5,
-		Assassinate = 6,
-		Escort = 7,
-		AdvancedAlliance = 8,
-		Manhunt = 12,
-		AdvancedWanted = 13
-	}
+    /// <summary>
+    /// Available game mode IDs included in session data.
+    /// </summary>
+    public enum GameMode
+    {
+        Menu = 1,
+        ChestCapture = 2,
+        Alliance = 3,
+        Wanted = 4,
+        Intro = 5,
+        Assassinate = 6,
+        Escort = 7,
+        AdvancedAlliance = 8,
+        Manhunt = 12,
+        AdvancedWanted = 13
+    }
 }
--- QuazalWV/RMC/Notifications/NotificationManager.cs
+++ QuazalWV/RMC/Notifications/NotificationManager.cs
@@ -124,5 +124,20 @@
                 ""
                 ).Send();
         }
+
+        public static void GameInviteSomething(ClientInfo receiverClient, uint senderPid, uint sessionId)
+        {
+            new NotificationEvent(
+                receiverClient,
+                0,
+                senderPid,
+                (uint)NotificationEventType.GameSession,
+                (uint)GameSessionNotificationSubtype.GameSessionNotif9,
+                0,
+                sessionId,
+                0,
+                ""
+                ).Send();
+        }
     }
 }
