702b414fd39d5a0590f4c2f408a2144a801316e9
michal-kapala <mim4k1@gmail.com>
2025-06-23 03:43:48 +0200

    msg: fix missing response

--- QuazalWV/RMC/MessageDeliveryService/MessageDeliveryService.cs
+++ QuazalWV/RMC/MessageDeliveryService/MessageDeliveryService.cs
@@ -3,32 +3,36 @@ using System.IO;
 
 namespace QuazalWV
 {
-	public static class MessageDeliveryService
-	{
-		public static void ProcessRequest(Stream s, RMCP rmc)
-		{
-			switch (rmc.methodID)
-			{
-				case 1:
-					rmc.request = new RMCPacketRequestMessageDeliveryService_DeliverMessage(s);
-					break;
-				default:
-					Log.WriteLine(1, $"[RMC MessageDelivery] Error: Unknown Method {rmc.methodID}", Color.Red);
-					break;
-			}
-		}
+    public static class MessageDeliveryService
+    {
+        public static void ProcessRequest(Stream s, RMCP rmc)
+        {
+            switch (rmc.methodID)
+            {
+                case 1:
+                    rmc.request = new RMCPacketRequestMessageDeliveryService_DeliverMessage(s);
+                    break;
+                default:
+                    Log.WriteLine(1, $"[RMC MessageDelivery] Error: Unknown Method {rmc.methodID}", Color.Red);
+                    break;
+            }
+        }
 
-		public static void HandleRequest(QPacket p, RMCP rmc, ClientInfo client)
-		{
-			RMCPResponse reply;
-			switch (rmc.methodID)
-			{
-				case 1:
-					break;
-				default:
-					Log.WriteLine(1, $"[RMC MessageDelivery] Error: Unknown Method {rmc.methodID}", Color.Red, client);
-					break;
-			}
-		}
-	}
+        public static void HandleRequest(QPacket p, RMCP rmc, ClientInfo client)
+        {
+            RMCPResponse reply;
+            switch (rmc.methodID)
+            {
+                case 1:
+                    reply = new RMCPResponseEmpty();
+                    var reqDeliver = (RMCPacketRequestMessageDeliveryService_DeliverMessage)rmc.request;
+                    Log.WriteLine(1, $"[RMC MessageDelivery] '{reqDeliver.Message.Body}' from {reqDeliver.Message.SenderName} to {reqDeliver.Message.RecipientId}", Color.Blue, client);
+                    RMC.SendResponseWithACK(client.udp, p, rmc, client, reply);
+                    break;
+                default:
+                    Log.WriteLine(1, $"[RMC MessageDelivery] Error: Unknown Method {rmc.methodID}", Color.Red, client);
+                    break;
+            }
+        }
+    }
 }
